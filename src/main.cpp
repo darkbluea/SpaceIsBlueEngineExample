fd6fe7f0949e5b33977ef0f90cb149822314a855
BlueaProjects <blueavideo@gmail.com>
2024-07-16 03:24:05 +0200

    first push

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <SpaceIsBlueEngine/Engine.h>
+
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+    SpaceIsBlueEngine::Engine::Engine engine;
+
+    return engine.Run();
+}
